84060e73d1f9945f0a90f602bec060d8b6bdf625
Alexandra <ralex2709@gmail.com>
2021-12-05 21:26:46 +0300

    'ntrcn'

--- tests/test_business_card.py
+++ tests/test_business_card.py
@@ -1,25 +1,31 @@
-from time import sleep
-
-from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
-from selene import be, have
+from selene import be
+from selene import command
 
 from base_test import BaseTest
 
 
 class TestBusinessCard(BaseTest):
-    password = '123456789'
-    phone = '(981)437-01-53'
+    fio_portal = 'Тестовый тест'
+    fio_not_portal = 'Токач Диана'
 
 
     def setup(self):
         super().setup()
         s('#test-services_nav').hover()
         s('#test-services_nav__businessCards').click()
-        assert browser.driver.current_url == 'https://maxitest.ru/services/business-card'
 
     def test_simple(self):
         s('#test-services_business-card__button').click()
-        # sleep(1)
+        s('.business-card-modal__body').should(be.visible)
+        s('.close-modal').click()
+
+        s('#test-services_business-card__fio').perform(command.js.scroll_into_view)
+        s('#test-services_business-card__fio').should(be.visible)
+        s('#test-services_business-card__fio').click()
+        s('#test-services_business-card__fio').clear()
+        s('#test-services_business-card__fio').click().set_value(self.fio_portal)
+        ss('.user-finder-option__name').element(0).click()
+        s('#test-services_business-card__button').click()
         s('.business-card-modal__body').should(be.visible)
         s('.close-modal').click()
